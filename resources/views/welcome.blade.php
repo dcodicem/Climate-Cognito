b5d59a85846c5ad9f82804dc280952a1481898d8
Lateh Larry <clatehlarry@gmail.com>
2025-06-02 02:57:26 +0100

    added homepage

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"
+        content="width=device-width,  minimum-scale=1, maximum-scale =1, user-scalable = no , shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <title>Climate Cognito</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        body {
+            background: #eee;
+        }
+
+        span {
+            font-size: 15px;
+        }
+
+        a {
+            text-decoration: none;
+            color: #0062cc;
+            border-bottom: 2px solid #0062cc;
+        }
+
+        .box {
+            padding: 60px 0px;
+        }
+
+        .box-part {
+            background: #FFF;
+            border-radius: 0;
+            padding: 60px 10px;
+            margin: 30px 0px;
+        }
+
+        .text {
+            margin: 20px 0px;
+        }
+
+        .fa {
+            color: #4183D7;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="box">
+    <div class="container">
+          <h5 class="section-title h1 text-center">Climate Cognito</h5>
+          
+     	<div class="row">
+			 
+			    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div class="box-part text-center">
+                        
+                        <i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
+                        
+						<div class="title">
+							<h4>SDG 2: Zero Hunger</h4>
+						</div>
+                        
+						<div class="text">
+							<span>By providing climate-resilient crop recommendations and adaptation strategies, the agent can help ensure food security and sustainable agriculture practices.</span>
+						</div>
+                        
+					 </div>
+				</div>	 
+				
+				 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div class="box-part text-center">
+					    
+					    <i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
+                    
+						<div class="title">
+							<h4>SDG 13: Climate Action</h4>
+						</div>
+                        
+						<div class="text">
+							<span>The agent's focus on climate change adaptation and resilience directly supports efforts to combat climate change and its impacts.</span>
+						</div>
+                        
+					 </div>
+				</div>	 
+				
+				 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+               
+					<div class="box-part text-center">
+                        
+                        <i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
+                        
+						<div class="title">
+							<h4>SDG 12: Responsible Consumption and Production</h4>
+						</div>
+                        
+						<div class="text">
+							<span>By promoting climate-resilient agriculture practices and sustainable crop recommendations, the agent can help reduce the environmental impact of farming, which aligns with the goals of SDG 12.</span>
+						</div>
+                        
+					 </div>
+				</div>	 
+
+                <div class="text-center">
+                    <a href="/username"> <h5 class="section-title h1 text-center">Start Chat</h5></a>
+                </div>
+				
+				
+		
+		</div>		
+    </div>
+</div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+</body>
+
+
+</html>
